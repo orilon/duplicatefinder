c97cd21d429c04689cb6f5aa0eb2c4c888f36d10
Chris Visser <chris@orilon.co.za>
2019-08-19 23:03:40 +0200

    added console app

---
+++ DuplicateFinder/Program.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DuplicateFinder
+{
+    class Program
+    {
+        class LineItem
+        {
+            public string FilePath { get; set; }
+            public int LineNumber { get; set; }
+        }
+        class DuplicateItem
+        {
+            public DuplicateItem()
+            {
+                LineItems = new List<LineItem>();
+            }
+            public string Key { get; set; }
+            public string Value { get; set; }
+            public int Count { get; set; }
+            public List<LineItem> LineItems { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            ConsoleColor _defaultColor = Console.ForegroundColor;
+
+        rerun:
+            sw.Restart();
+            Console.WriteLine("Please specify the directory where the files are:");
+            string _folder = Console.ReadLine();
+
+            Console.WriteLine("Please specify the file extensions of files you want inspected:");
+            string _extension = Console.ReadLine();
+            if (String.IsNullOrEmpty(_extension))
+                _extension = $"*";
+
+            Console.WriteLine("Please specify the content that will be looked for (key, etc):");
+            string _contains = Console.ReadLine();
+
+            string folder = System.IO.Directory.GetCurrentDirectory();
+            if (!String.IsNullOrEmpty(_folder) && System.IO.Directory.Exists(_folder))
+                folder = _folder;
+
+            Console.WriteLine($" analysing files for duplicates in {folder} ...");
+
+            List<String> files = new List<string>();
+            List<DuplicateItem> _duplicates = new List<DuplicateItem>();
+            int totalFiles = 0;
+            int totalDuplicates = 0;
+
+            foreach (var filepath in System.IO.Directory.GetFiles(folder, $"*.{_extension}", System.IO.SearchOption.AllDirectories))
+            {
+                totalFiles++;
+                int _linenumber = 0;
+                string line;
+                System.IO.StreamReader file = new System.IO.StreamReader(filepath);
+                while ((line = file.ReadLine()) != null)
+                {
+                    _linenumber++;
+                    if (line.Contains(_contains))
+                    {
+                        //string _key = $"{System.Math.Abs(filepath.GetHashCode())}-{System.Math.Abs(_contains.GetHashCode())}";
+                        string _key = $"{System.Math.Abs(line.GetHashCode())}";
+                        var _duplicate = _duplicates.Find(x => x.Key == _key) ?? null;
+                        if (_duplicate == null)
+                        {
+                            _duplicate = new DuplicateItem();
+                            _duplicate.Count = 1;
+                            _duplicate.LineItems = new List<LineItem>() { new LineItem() { FilePath = $"{filepath.Trim()}", LineNumber = _linenumber } };
+                            _duplicate.Key = _key;
+                            _duplicate.Value = line.Trim();
+                            _duplicates.Add(_duplicate);
+                        }
+                        else
+                        {
+                            _duplicate.LineItems.Add(new LineItem() { FilePath = $"{filepath.Trim()}", LineNumber = _linenumber });
+                            _duplicate.Count++;
+                        }
+                        totalDuplicates++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Here are all the files with duplicate {_contains}");
+            foreach (var _duplicate in _duplicates.FindAll(x => x.Count > 1))
+            {
+                Console.Write($"Found ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"{_duplicate.Count} duplicates");
+                Console.ForegroundColor = _defaultColor;
+                Console.Write($" of ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{_duplicate.Value}");
+                Console.ForegroundColor = _defaultColor;
+                foreach (var line in _duplicate.LineItems)
+                {
+                    Console.Write($" in: {line.FilePath} ");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"(line {line.LineNumber})");
+                    Console.ForegroundColor = _defaultColor;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Would you like to export these results? (Y)es or (N)o, default is (N)o.");
+            ConsoleKey _keyExport = Console.ReadKey(true).Key;
+            if (_keyExport == ConsoleKey.Y)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Results:");
+
+                Console.ForegroundColor = _defaultColor;
+                var _outputFile = $"duplicatefinder-{DateTime.Now.ToString("yyyyMMdd")}";
+
+                var _outputJson = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(folder), $"{_outputFile}.json");
+                var _json = JsonConvert.SerializeObject(_duplicates);
+                Console.WriteLine($" creating json output to {_outputJson} ...");
+                System.IO.File.WriteAllText(_outputJson, _json);
+
+                var _outputYaml = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(folder), $"{_outputFile}.yaml");
+                var serializer = new YamlDotNet.Serialization.Serializer();
+                //serializer.Serialize(Console.Out, _duplicates);
+                Console.WriteLine($" creating yaml output to {_outputYaml} ...");
+                serializer.Serialize(System.IO.File.CreateText(_outputYaml), _duplicates);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($" analysis of {totalDuplicates} duplicates in {totalFiles} files completed in {sw.Elapsed}");
+            Console.ForegroundColor = _defaultColor;
+            Console.WriteLine($"-------------------------------------------------------------------------------------");
+            sw.Reset();
+            goto rerun;
+        }
+    }
+}
--- DuplicateFinder/readme.md
+++ DuplicateFinder/readme.md
@@ -1,4 +1,5 @@
-# duplicatefinder
-Utility to identify duplicate lines in specified file types (by extension)
+﻿# Introduction
+
 This application was a quick 'throw together' to help identify duplicate lines in ARM templates for our Azure deployments.
 
+Feel free to use it as you need.
