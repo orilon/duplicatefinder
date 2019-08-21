49acef7dd552dfd3d80ddc308e7756b79895f925
Chris Visser <chris@orilon.co.za>
2019-08-21 12:56:48 +0200

    adding verbose and debug features to application

--- DuplicateFinder/Program.cs
+++ DuplicateFinder/Program.cs
@@ -26,25 +26,72 @@ namespace DuplicateFinder
         }
         static void Main(string[] args)
         {
+            bool _verbose = args.Contains("-v") || args.Contains("--verbose");
+            bool _debug = args.Contains("-d") || args.Contains("--debug");
+
             Stopwatch sw = new Stopwatch();
             ConsoleColor _defaultColor = Console.ForegroundColor;
 
         rerun:
             sw.Restart();
+            Console.WriteLine("Please specify the minimum count as duplicates (default is 2):");
+            if (_verbose)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($" eg. this will be used to return the items that match and exceed a count of \"x\" items.");
+                Console.ForegroundColor = _defaultColor;
+            }
+
+            int _minCount = 2;
+            string? _minCountGiven = Console.ReadLine();
+            if (!String.IsNullOrEmpty(_minCountGiven) && int.TryParse(_minCountGiven, out _minCount))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($" successfully changed minimum count for duplicate validations to {_minCount}");
+                Console.ForegroundColor = _defaultColor;
+            }
+            if (_debug)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine($" debug: mincount set to {_minCount}");
+                Console.ForegroundColor = _defaultColor;
+            }
+
             Console.WriteLine("Please specify the directory where the files are:");
             string _folder = Console.ReadLine();
+
+            string folder = System.IO.Directory.GetCurrentDirectory();
+            if (!String.IsNullOrEmpty(_folder) && System.IO.Directory.Exists(_folder))
+                folder = _folder;
+
+            if (_debug)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine($" debug: folder is set to {folder}");
+                Console.ForegroundColor = _defaultColor;
+            }
 
             Console.WriteLine("Please specify the file extensions of files you want inspected:");
             string _extension = Console.ReadLine();
             if (String.IsNullOrEmpty(_extension))
                 _extension = $"*";
 
+            if (_debug)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine($" debug: files with the {_extension} extension will be analysed");
+                Console.ForegroundColor = _defaultColor;
+            }
+
             Console.WriteLine("Please specify the content that will be looked for (key, etc):");
             string _contains = Console.ReadLine();
 
-            string folder = System.IO.Directory.GetCurrentDirectory();
-            if (!String.IsNullOrEmpty(_folder) && System.IO.Directory.Exists(_folder))
-                folder = _folder;
+            if (_debug)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine($" debug: lines containing {_contains} will be analysed");
+                Console.ForegroundColor = _defaultColor;
+            }
 
             Console.WriteLine($" analysing files for duplicates in {folder} ...");
 
@@ -55,6 +102,13 @@ namespace DuplicateFinder
 
             foreach (var filepath in System.IO.Directory.GetFiles(folder, $"*.{_extension}", System.IO.SearchOption.AllDirectories))
             {
+                if (_debug)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($" debug: analysing file \"{System.IO.Path.GetFileName(filepath)}\"");
+                    Console.ForegroundColor = _defaultColor;
+                }
+
                 totalFiles++;
                 int _linenumber = 0;
                 string line;
@@ -88,7 +142,7 @@ namespace DuplicateFinder
 
             Console.WriteLine($"");
             Console.WriteLine($"Here are all the files with duplicate {_contains}");
-            foreach (var _duplicate in _duplicates.FindAll(x => x.Count > 1))
+            foreach (var _duplicate in _duplicates.FindAll(x => x.Count >= _minCount))
             {
                 Console.Write($"Found ");
                 Console.ForegroundColor = ConsoleColor.Red;
